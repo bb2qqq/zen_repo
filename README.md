b7c94898b9c9c3f505b670fcfd9d664949e9376c
Zeng.Juchen <zengjuchen@gmail.com>
2015-11-23 15:25:46 +0800

    整理git repo

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Today is:
 * 在所有临时代码文件（非永久性，可动态生成）的文件名里里加入 关键词`TEMPZ`
 
 > [OSX 的文件命名是case-insensative的](http://apple.stackexchange.com/questions/22297/is-bash-in-osx-case-insensitive), 请注意这点，Linux的文件命名是case-sensative的，此外，如果你的filesystem使用UFS(Unix File System), 它也是case-sensative的，OSX用的是HFS+
+
+BlackHole文件夹用来放置久不更新或obsolete的文件
--- tech_lab/utils/shell_scripts/synchronise_mac_config.sh
+++ tech_lab/utils/shell_scripts/synchronise_mac_config.sh
@@ -1,13 +1,13 @@
 # I'd better use rsync instead of cp one day
-cp ~/.bash_profile  ~/zen/pythonstudy/tech_lab/config/OSX/bash_profile
+cp ~/.bash_profile  ~/zen/zen_repo/tech_lab/config/OSX/bash_profile
 
 # filter
 grep -v "ssh" ~/.bashrc | grep -v "admin" > ~/tmp
-cp ~/tmp ~/zen/pythonstudy/tech_lab/config/OSX/bashrc
-cp ~/.vimrc ~/zen/pythonstudy/tech_lab/config/CrossPlatform/vimrc
-cp ~/.vimrc ~/zen/pythonstudy/tech_lab/config/OSX/vimrc
-cp ~/local_vim_scripts.vim ~/zen/pythonstudy/tech_lab/config/OSX/local_vim_scripts.vim
-cp ~/.vim/plugin/* /Users/zen1/zen/pythonstudy/tech_lab/vim_skill/my_plugin
+cp ~/tmp ~/zen/zen_repo/tech_lab/config/OSX/bashrc
+cp ~/.vimrc ~/zen/zen_repo/tech_lab/config/CrossPlatform/vimrc
+cp ~/.vimrc ~/zen/zen_repo/tech_lab/config/OSX/vimrc
+cp ~/local_vim_scripts.vim ~/zen/zen_repo/tech_lab/config/OSX/local_vim_scripts.vim
+cp ~/.vim/plugin/* /Users/zen1/zen/zen_repo/tech_lab/vim_skill/my_plugin
 
 # Universal code
-cp /Users/zen1/zen/automation/General/new_system/general_gadget/* /Users/zen1/zen/pythonstudy/tech_lab/my_work/zen_system/
+cp /Users/zen1/zen/automation/General/new_system/general_gadget/* /Users/zen1/zen/zen_repo/tech_lab/my_work/zen_system/
